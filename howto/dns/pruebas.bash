d984fc7d0c3c241a61fdb2adef0174d0f283f846
Renich Bon Ciric <renich@woralelandia.com>
2021-05-15 13:12:55 -0500

    dns: re-escribí las pruebas para evitar caracteres problemáticos

---
+++ howto/dns/pruebas.bash
@@ -0,0 +1,9 @@
+# buscar el dominio principal
+dig example.tld
+
+# buscar los subdominios
+dig ns1.example.tld
+dig mail1.example.tld
+
+# buscar uno no existente y verificar que lo envía a 192.168.77.10
+dig chuchuwa.example.tld
